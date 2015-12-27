ce195739f0bb564a3266336ba1e59dbf120f9305
drafterleo <drafterleo@gmail.com>
2015-12-28 00:10:45 +0300

    from home 28.12.2015

---
+++ grad_descent.py
@@ -0,0 +1,19 @@
+import random
+
+def factorize_number(num, alpha=0.001):
+    p = random.random() * random.randint(1, int(num))
+    q = random.random() * random.randint(1, int(num))
+    err = num - p*q
+    while err ** 2 > 0.0001:
+        print ("[num: %f] p = %f, q = %f, p*q = %f, err = %f" % (num, p, q, p*q, err ** 2))
+        pt = p
+        p += 2*err*q*alpha
+        q += 2*err*pt*alpha
+        err = num - p*q
+    return p, q, p*q, err ** 2
+
+
+if __name__ == '__main__':
+    print(factorize_number(18.7))
+
+
--- recommender.py
+++ recommender.py
@@ -1,6 +1,6 @@
 import numpy as np
 from math import sqrt
-from numba import jit
+from numba import autojit, double, int_
 
 def gen_data_table(n_users, n_items):
     p0 = 0.4
@@ -23,14 +23,14 @@ def get_vector_mean(vector):
     return vector[vector > 0].mean()
 
 
-def get_inersection_of_vectors(vector_a, vector_b):
+def get_intersection_of_vectors(vector_a, vector_b):
     joints = 0
     for i in range(min(len(vector_a), len(vector_b))):
         if vector_a[i] != 0 and vector_b[i] != 0:
             joints += 1
     return joints
 
-
+@autojit
 def sim_cos(vector_a, vector_b):
     dot_prod = 0.0
     magn_a = 0.0
@@ -38,7 +38,7 @@ def sim_cos(vector_a, vector_b):
     mean_a = get_vector_mean(vector_a)
     mean_b = get_vector_mean(vector_b)
     min_vector_len = min(len(vector_a), len(vector_b))
-    joint_factor = get_inersection_of_vectors(vector_a, vector_b) / min_vector_len
+    joint_factor = get_intersection_of_vectors(vector_a, vector_b) / min_vector_len
     for i in range(min_vector_len):
         if vector_a[i] != 0 and vector_b[i] != 0:
             a = (vector_a[i] - mean_a) * joint_factor
@@ -87,6 +87,7 @@ def base_line_estimation(data, user, item, avg=float("nan")):
    avg_user = np.sum(user_vector)/np.count_nonzero(user_vector)
    avg_item = np.sum(item_vector)/np.count_nonzero(item_vector)
    return avg_user + avg_item - avg
+
 
 def recommendations_by_items(data, person, predict_items=[], max_similar_items=7, allow_base_line=False):
     user_vector = get_user_vector(data, person)
@@ -159,66 +160,87 @@ def get_RMSE(data, user):
         rmse = sqrt(rmse / len(recs))
     return rmse, recs
 
-# @jit
-def matrix_factorization(R, P, Q, K, steps=2000, alpha=0.0008, beta=0.02):
-    Q = Q.T
-    R_rows, R_cols = R.shape
+def matrix_rmse(src_matrix, clc_matrix):
+    if src_matrix.shape != clc_matrix.shape or src_matrix.size == 0:
+        return float("inf")
+    rmse = 0.0
+    for i in range(src_matrix.size):
+        if src_matrix.flat[i] > 0:
+            rmse += (src_matrix.flat[i] - clc_matrix.flat[i]) ** 2
+    return sqrt(rmse / src_matrix.size)
+
+
+# https://gist.github.com/jhemann/5584536
+@autojit(locals={'step': int_, 'e': double, 'alpha': double})
+def matrix_factorization(R, P, Q, K):
+    steps = 5000
+    alpha = 0.0002
+    beta = 0.02
+    half_beta = beta / 2.0
+    N, M = R.shape
     e = 0.0
     for step in range(steps):
-        for i in range(R_rows):
-            for j in range(R_cols):
-                if R[i][j] > 0:
-                    eij = R[i][j] - np.dot(P[i, :], Q[: ,j])
+        for i in range(N):
+            for j in range(M):
+                if R[i,j] > 0:
+                    eij = R[i,j]
+                    for p in range(K):
+                        eij -= P[i,p] * Q[j,p]
                     for k in range(K):
-                        p_ik = P[i][k]
-                        q_kj = Q[k][j]
-                        P[i][k] += alpha * (2 * eij * q_kj - beta * p_ik)
-                        Q[k][j] += alpha * (2 * eij * p_ik - beta * q_kj)
-    #     e = 0.0
-    #     for i in range(R_rows):
-    #         for j in range(R_cols):
-    #             if R[i][j] > 0:
-    #                 e += (R[i][j] - np.dot(P[i, :], Q[:, j])) ** 2
-    #                 for k in range(K):
-    #                     e += (beta/2) * (P[i][k] ** 2) + (Q[k][j] ** 2)
-    #     if e < 0.001:
-    #         break
-    # print(e)
-    return np.dot(P,Q) # P, Q.T
-
+                        pik = P[i,k]
+                        qjk = Q[j,k]
+                        P[i,k] += alpha * (2 * eij * qjk - beta * pik)
+                        Q[j,k] += alpha * (2 * eij * pik - beta * qjk)
+        e = 0.0
+        for i in range(N):
+            for j in range(M):
+                if R[i,j] > 0:
+                    rij = R[i,j]
+                    for p in range(K):
+                        rij -= P[i,p] * Q[j,p]
+                    e = e + rij * rij
+                    for k in range(K):
+                        e += half_beta * (P[i,k]*P[i,k] + Q[j,k]*Q[j,k])
+        if e < 0.001:
+            break
+    return P, Q, step + 1, e
 
 def test_recommender():
-    rating_data = gen_data_table(n_users=10, n_items=18)
-    print(rating_data)
+    data = gen_data_table(n_users=20, n_items=40)
+    np.set_printoptions(linewidth=170)
+    print(data)
 
     # print(get_item_vector(rating_data, 1))
     # print(get_item_vector(rating_data, 3))
     # print(item_sim(rating_data, 1, 3))
 
     curr_user = 0
-    for similar_user in most_similar_users(rating_data, curr_user, 5):
+    for similar_user in most_similar_users(data, curr_user, 5):
         print("user [%d] (%f)" % (similar_user[1], similar_user[0]))
-        curr_user_vector = get_user_vector(rating_data, curr_user)
-        similar_user_vector = get_user_vector(rating_data, similar_user[1])
+        curr_user_vector = get_user_vector(data, curr_user)
+        similar_user_vector = get_user_vector(data, similar_user[1])
         print("   ", curr_user_vector, get_vector_mean(curr_user_vector))
         print("   ", similar_user_vector, get_vector_mean(similar_user_vector))
 
     print("\n item-item recommendations for user %d: \n" % curr_user,
-          recommendations_by_items(rating_data, curr_user, max_similar_items=10))
+          recommendations_by_items(data, curr_user, max_similar_items=7))
     print("\n base line recommendations for user %d: \n" % curr_user,
-          recommendations_by_items(rating_data, curr_user, max_similar_items=10, allow_base_line=True))
-    print("\n RMSE for user %d: \n" % curr_user, get_RMSE(rating_data, curr_user))
+          recommendations_by_items(data, curr_user, max_similar_items=7, allow_base_line=True))
+    print("\n RMSE for user %d: \n" % curr_user, get_RMSE(data, curr_user))
 
     print("\n user-user recommendations for user %d: \n" % curr_user,
-          recommendations_by_users(rating_data, curr_user, max_similar_users=10))
+          recommendations_by_users(data, curr_user, max_similar_users=10))
 
-    N, M = rating_data.shape
-    K = 3
+    N, M = data.shape
+    K = 8
     P = np.random.rand(N, K)
     Q = np.random.rand(M, K)
 
     #matrix_factorization(rating_data, P, Q, K, steps=-1)
-    print("\n matrix factorization: \n", matrix_factorization(rating_data, P, Q, K))
+    P, Q, step, err = matrix_factorization(data, P, Q, K)
+    R = np.dot(P, Q.T)
+    np.set_printoptions(precision=3, suppress=True, linewidth=75)
+    print("\n matrix factorization (err = %f, step = %d, rmse = %f): \n" % (err, step, matrix_rmse(data, R)), R)
 
 
 if __name__ == '__main__':
