ffb3a110555b248eb8c8da0edeed0045bea1cd0b
drafterleo <drafterleo@gmail.com>
2015-12-28 19:08:02 +0300

    home 28.12.2015

--- grad_descent.py
+++ grad_descent.py
@@ -1,13 +1,24 @@
 import random as rnd
 import numpy as np
 
+def factorize_number_0(num, alpha=0.001):
+    p = rnd.random() * rnd.randint(1, int(num))
+    q = rnd.random() * rnd.randint(1, int(num))
+    err = num - p*q
+    while err ** 2 > 0.0001:
+        print ("[num: %f] p = %f, q = %f, p*q = %f, err = %f" % (num, p, q, p*q, err ** 2))
+        pt = p
+        p += 2*err*q*alpha
+        q += 2*err*pt*alpha
+        err = num - p*q
+    return p, q, p*q, err ** 2
 
-def random_vector(about_num, vector_length=5):
-    return np.array([rnd.random() for _ in range(vector_length)])
+def random_vector(length=5):
+    return np.array([rnd.random() for _ in range(length)])
 
 def factorize_number(num, alpha=0.001, K=3):
-    p = random_vector(int(num), vector_length=K)
-    q = random_vector(int(num), vector_length=K)
+    p = random_vector(length=K)
+    q = random_vector(length=K)
     err = num - np.dot(p, q)
     while err ** 2 > 0.0001:
         #print ("[num: %f] p = %f, q = %f, p*q = %f, err = %f" % (num, p, q, p*q, err ** 2))
